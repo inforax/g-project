fe847df0b36c44fdccda2b6bda184ca98404336b
KayzerCode <zlo.alien@gmail.com>
2023-04-23 10:00:26 +0300

    admin

--- admin.ts
+++ admin.ts
@@ -1,16 +1,5 @@
 // Admin
-export namespace Admin {
- // Create, Update
- export class Input {
-  name: string
-  email: string
- }
- // Delete
- export class Delete {
-  id: number
- }
- // Get (Read)
- export class Output {
-  id: number
- }
+export class Admin {
+ name: string
+ email: string
 }
